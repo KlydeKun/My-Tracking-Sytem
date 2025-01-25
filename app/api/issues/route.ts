b9bfa7489829cf384faea3229c4aa3320c0f5f0c
KlydeKun <kylelerio15@gmail.com>
2025-01-26 03:47:22 +0800

    Add a markdown editor

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -8,15 +8,30 @@ const createIssueSchema = z.object({
 });
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
-  if (!validation.success) {
-    return NextResponse.json(validation.error.errors, { status: 400 });
+  try {
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body);
+    if (!validation.success) {
+      return NextResponse.json(validation.error.errors, { status: 400 });
+    }
+
+    const newIssue = await prisma.issue.create({
+      data: { title: body.title, description: body.description },
+    });
+
+    return NextResponse.json(newIssue, { status: 201 });
+  } catch (error) {
+    console.error("Error creating issue:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
+}
 
-  const newIssue = await prisma.issue.create({
-    data: { title: body.title, description: body.description },
-  });
-
-  return NextResponse.json(newIssue, { status: 201 });
+export async function GET() {
+  return NextResponse.json(
+    { message: "GET requests are not supported on this route" },
+    { status: 405 }
+  );
 }
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,18 @@
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+"use client"
+
+import { Button, TextField } from "@radix-ui/themes";
+import { SimpleMdeReact } from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import React from "react";
 
 const NewIssuePage = () => {
   return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder='Title' />
-      <TextArea placeholder='Description' />
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Title" />
+      <SimpleMdeReact placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
-  )
-}
+  );
+};
 
-export default NewIssuePage
+export default NewIssuePage;
