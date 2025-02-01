a85733ca6d25c553c941aea7fde353b548f26640
KlydeKun <kylelerio15@gmail.com>
2025-02-01 20:38:09 +0800

    Build the Edit Issue Page
    Refactor: Build IssueForm component for NewIssue & EditIssue
    Add loading skeleton

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import IssueForm from "@/app/issues/components/IssueForm";
+import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
 
-const EditIssuePage = () => {
-  return (
-    <div>EditIssuePage</div>
-  )
+interface Props {
+  params: { id: string };
 }
 
-export default EditIssuePage
+const EditIssuePage = async ({ params }: Props) => {
+  const editIssue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!editIssue) notFound();
+
+  return <IssueForm issue={editIssue} />;
+};
+
+export default EditIssuePage;
---
+++ app/issues/components/IssueForm.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import ErrorMessage from "@/app/components/ErrorMessage";
+import { createIssueSchema } from "@/app/validationSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Issue } from "@prisma/client";
+import { Button, Callout, Spinner, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const SimpleMdeReact = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+const IssueForm = ({ issue }: { issue?: Issue }) => {
+  const { push } = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const [error, setError] = useState("");
+  const [loadingSubmit, setLoadingSubmit] = useState(false);
+
+  const createSubmitIssue = handleSubmit(async (data: IssueFormData) => {
+    try {
+      setLoadingSubmit(true);
+      await axios.post("/api/issues", data);
+      push("/issues");
+    } catch {
+      setLoadingSubmit(false);
+      setError("An unexpected error occurred. Please try again.");
+    }
+  });
+
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-3" onSubmit={createSubmitIssue}>
+        <TextField.Root
+          defaultValue={issue?.title}
+          placeholder="Title"
+          {...register("title")}
+        />
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          defaultValue={issue?.description}
+          render={({ field }) => (
+            <SimpleMdeReact placeholder="Description" {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button disabled={loadingSubmit}>
+          Submit New Issue
+          <Spinner loading={loadingSubmit} />
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default IssueForm;
