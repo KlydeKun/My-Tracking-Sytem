f0f4979ac3f634ce4a0e284c7b28fc8f500091d6
KlydeKun <kylelerio15@gmail.com>
2025-03-02 18:28:54 +0800

    Add navigation bar with user authentication and avatar display

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,14 @@
 "use client";
 
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+} from "@radix-ui/themes";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -8,6 +16,7 @@ import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   const links = [
     { label: "Dashboard", href: "/" },
@@ -16,25 +25,73 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex space-x-10">
-        {links.map((link) => (
-          <Link
-            key={link.href}
-            className={classNames({
-              "text-zinc-900 font-medium": link.href === currentPath,
-              "text-zinc-500": link.href !== currentPath,
-              "hover:text-zinc-800 transition-colors": true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
-        ))}
-      </ul>
+    <nav className="border-b mb-5 px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <AiFillBug />
+            </Link>
+            <ul className="flex space-x-10">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={classNames({
+                      "text-zinc-900 font-medium": link.href === currentPath,
+                      "text-zinc-500": link.href !== currentPath,
+                      "hover:text-zinc-800 transition-colors": true,
+                    })}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    className="cursor-pointer"
+                    radius="full"
+                    src={session.user!.image!}
+                    fallback="?"
+                    referrerPolicy="no-referrer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    {session.user!.email!}
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Log out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+              // <DropdownMenu.Root>
+              //   <DropdownMenu.Trigger>
+              // <Avatar
+              //   src={session.user!.image!}
+              //   fallback="U"
+              //   size="2"
+              //   radius="full"
+              // />
+              //   </DropdownMenu.Trigger>
+              // <DropdownMenu.Content>
+              //   <DropdownMenu.Label>
+              //     {session.user!.email!}
+              //   </DropdownMenu.Label>
+              // </DropdownMenu.Content>
+              // </DropdownMenu.Root>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Log in</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
